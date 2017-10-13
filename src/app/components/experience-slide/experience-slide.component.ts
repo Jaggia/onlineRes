4a22aeeb6c238e8d7b6595df9bfaf85c5cea6c67
jaggia <anadi.jaggia@gmail.com>
2017-10-12 21:57:09 -0400

    first commit

--- src/app/components/experience-slide/experience-slide.component.ts
+++ src/app/components/experience-slide/experience-slide.component.ts
@@ -8,6 +8,18 @@ import {WOW} from 'wowjs';
 })
 export class ExperienceSlideComponent implements OnInit {
 
+  events = [{
+    badgeClass: 'info',
+    badgeIconClass: 'glyphicon-check',
+    title: 'First heading',
+    content: 'Some awesome content.'
+  }, {
+    badgeClass: 'warning',
+    badgeIconClass: 'glyphicon-credit-card',
+    title: 'Second heading',
+    content: 'More awesome content.'
+  }];
+
   constructor() { }
 
   ngOnInit() {
